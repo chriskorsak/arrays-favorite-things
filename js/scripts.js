f5298cebbfa8c567cfc7f435294056ef7b52cbe6
chriskorsak <chris@chriskorsak.com>
2020-05-11 19:21:16 -0700

    re-write js script to use loops instead of repeating code. Update output heading to read 'your favorites'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
   $("form#favorites").submit(function() {
-    var color = $("input#color").val();
-    var food = $("input#food").val();
-    var season = $("input#season").val();
-    var ledzeppelin = $("input#ledzeppelin").val();
-    var cocktail = $("input#cocktail").val();
-
-    var favoriteThings = [color, food, season, ledzeppelin, cocktail];
-    var selects = [];
-    selects.push(favoriteThings[0]);
-    selects.push(favoriteThings[1]);
-    selects.push(favoriteThings[2]);
-
-    $("ul#selectFavorites").append('<li>' + selects[0] + '</li>');
-    $("ul#selectFavorites").append('<li>' + selects[1] + '</li>');
-    $("ul#selectFavorites").append('<li>' + selects[2] + '</li>');
-
+    const favoriteThings = ["color", "food", "season", "ledzeppelin", "cocktail"];
+    const selects = favoriteThings.map(function(favoriteThing) {
+      return $("input#" + favoriteThing).val();
+    });
+    selects.forEach(function(select) {
+      $("#selectFavorites").append("<li>" + select + "</li>");
+    });
     $("#output").show();
     event.preventDefault();
   });
